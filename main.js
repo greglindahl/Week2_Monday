58975b7a2ed6c3e38b04134997ddb2d0ab56d50a
greglindahl <greglindahl410@gmail.com>
2015-09-23 06:31:06 -0600

    Completed all questions

--- main.js
+++ main.js
@@ -87,9 +87,10 @@ console.log(invitees.children[2].allergies[0]);
 
 // 9. Create a string that reads Lauren's dinner choice in a sentence.
 console.log('Question 9');
-var laurenMenu = "Lauren's appetizer is " + invitees.adults[2].dinnerChoice[0] + ". She will be eating " + invitees.adults[2].dinnerChoice[1] + " for dinner
-and has selected " + invitees.adults[2].dinnerChoice[2] + " as her dessert.";
+var laurenMenu = "Lauren\'s appetizer is " + invitees.adults[2].dinnerChoice[0] + ". She will be eating " + invitees.adults[2].dinnerChoice[1] + " for dinner and has selected " + invitees.adults[2].dinnerChoice[2] + " as her dessert.";
 console.log(laurenMenu);
+
+
 // Desired result: "Lauren's appetizer is _______. She will be eating ______ for dinner
 // and has selected ________ as her dessert."
 
@@ -152,9 +153,13 @@ console.log(books[0].bangkokHaunts.author.length);
 
 // 16. To each book object variable, add a key of "dateRead" with a value of the year you read the book.
 console.log('Question 16');
-books[0].youHadMeAtWoof.dateRead = 1984;
-books[0].theCatcherInTheRye.dateRead = 1994;
-books[0].bangkokHaunts.dateRead = 2000;
+for (i = 0; i < books[0].length; i++) {
+  year = 1980;
+  books[i].dateRead = year + 5;
+}
+// books[0].youHadMeAtWoof.dateRead = 1984;
+// books[0].theCatcherInTheRye.dateRead = 1994;
+// books[0].bangkokHaunts.dateRead = 2000;
 console.log(books);
 // (Go ahead and make this up, too. Get wild.)
 
@@ -166,3 +171,163 @@ console.log(books);
 // Bonus: Explain, in your own words, the difference between Literal Notation and Constructor.
 console.log('Question Bonus');
 console.log('Object Literals serve as a single object that you will build.  A Constructor Object serves a blueprint for many instances of that object.  For example I may need to create many kinds of cars from a car object and then add custom properties to it.');
+
+// 17. Let's play a game. Create an object (like we did with the bike in class) and assign it at least five properties.
+// The class will guess what your object is based on it's properties.
+console.log('Question 17');
+var mysteryObject = {
+  hasTeeth: true,
+  habitat: 'ocean',
+  color: 'stripes',
+  diet: ['fish', 'tires', 'whatever'],
+  manEater: true
+};
+console.log(mysteryObject);
+
+// You're a zookeeper and keeping a detailed log of each of the animals in your zoo.
+// 18. Create an empty object called "zoo"
+console.log('Question 18');
+var zoo = {};
+console.log(zoo);
+
+// 19. Create assign three animals as your keys in the "zoo" object. The values of the animals should be empty objects.
+console.log('Question 19');
+zoo.lion = {};
+zoo.tiger = {};
+zoo.bear = {};
+console.log(zoo);
+
+// 20. Within each animal empty object, assign a key/value pair of "timeSlept" and a number indicating the hours of sleep each animal got. (Make this up, obvi)
+console.log('Question 20');
+zoo.lion.timeSlept = 10;
+zoo.tiger.timeSlept = 8;
+zoo.bear.timeSlept = 14;
+console.log(zoo);
+
+// 21. Within each animal object, assign a object pair consisting of a key called "meals" and an empty array as the value.
+console.log('Question 21');
+// for(i = 0; i < zoo[0].length) {
+//   var meals = 2;
+//   zoo[i].meals = meals + 2;
+// }
+
+zoo.lion.meals = [];
+zoo.tiger.meals = [];
+zoo.bear.meals = [];
+console.log(zoo);
+
+
+// 22. Within each animal's meals array, create two objects with keys of "monday" and "wednesday". You only feed those jerks twice a week. It's fine.
+console.log('Question 22');
+zoo.lion.meals.monday = {};
+zoo.tiger.meals.monday = {};
+zoo.bear.meals.monday = {};
+zoo.lion.meals.wednesday = {};
+zoo.tiger.meals.wednesday = {};
+zoo.bear.meals.wednesday = {};
+console.log(zoo);
+
+// 23. In the individual days of the week, create an object with the key of each meal ("breakfast", "lunch", "dinner") and a made up value of what they ate.
+console.log('Question 23');
+zoo.lion.meals = [ {
+  monday: {
+    breakfast: "Meat",
+    lunch: "More Meat",
+    dinner: "Your Mom"
+  }
+},
+{
+  wednesday: {
+    breakfast: "Fish",
+    lunch: "More Fish",
+    dinner: "Zookeeper"
+  }
+}]
+zoo.tiger.meals = [ {
+  monday: {
+    breakfast: "Salmon",
+    lunch: "More Salmon",
+    dinner: "Your Mom"
+  }
+},
+{
+  wednesday: {
+    breakfast: "Fish",
+    lunch: "More Fish",
+    dinner: "Zookeeper"
+  }
+}]
+zoo.bear.meals = [ {
+  monday: {
+    breakfast: "Plants",
+    lunch: "More Plants",
+    dinner: "Your Mom"
+  }
+},
+{
+  wednesday: {
+    breakfast: "Salmon",
+    lunch: "More Salmon",
+    dinner: "Zookeeper"
+  }
+}]
+
+// That array should look like this (within the meals object, within the animal object, within the zoo object):
+  [ { monday: {
+    breakfast: "cheetos on top of greek yogurt",
+    lunch: "6 gallons of Diet Coke",
+    dinner: "leftover lasagna"
+  }},
+   {wednesday: {
+    breakfast: "cashews, diced apples, tuna, grapes, BubbleYum",
+    lunch: "a ham and cheddar lunchable",
+    dinner: "I put my thing down, flip it, and reverse it"
+  }}]
+
+// 24. Damn it. The night zookeeper just informed you that your second animal took a nap right before bed time. Add two hours to his "timeSlept" value.
+console.log('Question 24');
+zoo.tiger.timeSlept = zoo.tiger.timeSlept + 2;
+console.log(zoo.tiger.timeSlept);
+
+// 25. To each animal object, add a "favoriteActivities" key with the value being an array of three of his/her favorite activities.
+console.log('Question 25');
+zoo.lion.favoriteActivities = ['Eating', 'Lying Around', 'Eating Zookeepers'];
+zoo.tiger.favoriteActivities = ['Eating', 'Lying Around', 'Eating Fish'];
+zoo.bear.favoriteActivities = ['Eating', 'Hybernating', 'Swimming'];
+console.log(zoo);
+
+// 26. Your first animal just tossed his lunch at the zoo visitors. Change his monday lunch value to an empty string.
+console.log('Question 26');
+zoo.lion.meals.monday.lunch = '';
+console.log(zoo.lion.meals.monday.lunch);
+
+// 27. You know your "zoo" object? The owner wants to add zookeepers to the object, too. But before we do that, we need to make sure
+// the animals stay separate from the zookeepers. To do this, create an object within the "zoo" object. The key will be "animals" and the value will be an array
+// containing the three animals you already defined.
+console.log('Question 27');
+zoo.animals = ['lion', 'tiger', 'bear'];
+console.log(zoo.animals);
+
+// 28. To the "zoo" object, add an object named "zookeepers" with the value being and empty object.
+console.log('Question 28');
+zoo.zookeepers = {};
+console.log(zookeepers);
+
+// 29. To the empty "zookeepers" object, add two objects with their keys being two names and their values being an empty object.
+console.log('Question 29');
+
+// 30. This joker wants you to add the "hireDate" for each zookeeper. Within the empty array attached to each name, assign a key/value pair of
+// "hireDate" and the value being a string of hire date.
+console.log('Question 30');
+zoo.zookeepers = [{
+  Jim: {
+    hireDate: 'July 5th, 2000'
+  },
+  Jane: {
+    hireDate: 'June 12, 1989'
+  }
+}]
+
+
+
+
